8631325de06b009eac079622332e3d6054872158
Raphael <veauville.raphael@gmail.com>
2021-08-15 11:28:11 +0200

    Update

---
+++ docs/typedef/EventsHandlerOptions.md
@@ -0,0 +1,5 @@
+# HandlersOptions
+
+| Name      | Type                                                                                              | Description                      | Default | Required |
+| --------- | ------------------------------------------------------------------------------------------------- | -------------------------------- | ------- | -------- |
+| directory | [String](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String) | The main directory of the events |         | Yes      |
--- test/index.js
+++ test/index.js
@@ -1,22 +1,6 @@
-const { ShewenyClient, CommandsHandler } = require('../build/index');
+const { ShewenyClient } = require('../build/index');
 
 
-
-const client = new ShewenyClient({ intents: 'GUILDS' })
-// client.loadAll().then(r => console.log(r))
-
-client.login('Njg5MjExNjEwMzA0MzQ4MzMx.Xm_kVA.Q7U_VPFx_9T0Qb_mDh1Qd6fv88w')
-client.on('interactionCommandCreate', () => {
-	console.log('Good event');
+const client = new ShewenyClient({
+	intents: ['GUILDS', 'GUILDS_MESSAGES'],
 })
-const cmdHandler = new CommandsHandler(client, 'commands');
-cmdHandler.loadAll('commands').then(r => console.log(r))
-// const { Client } = require('discord.js');
-// class MyClient extends Client {
-// 	constructor(options) {
-// 		super(options)
-// 	}
-// }
-
-// const client = new MyClient({ intents: 'GUILDS' })
-// client.login('Njg5MjExNjEwMzA0MzQ4MzMx.Xm_kVA.Q7U_VPFx_9T0Qb_mDh1Qd6fv88w')
