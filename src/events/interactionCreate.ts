9c13cb95b96a74b9323630a99ffea16f37b93edf
Natchi <nathan.caron@natchi.fr>
2021-08-17 15:43:01 +0200

    Correction formatting file

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -2,7 +2,8 @@ import type { Interaction } from "discord.js";
 import type { ShewenyClient } from "../index";
 
 export default function run(client: ShewenyClient, interaction: Interaction) {
-  if (interaction.isButton()) return client.emit("interactionButtonCreate", interaction);
+  if (interaction.isButton()) 
+    return client.emit("interactionButtonCreate", interaction);
   if (interaction.isCommand())
     return client.emit("interactionCommandCreate", interaction);
   if (interaction.isContextMenu())
