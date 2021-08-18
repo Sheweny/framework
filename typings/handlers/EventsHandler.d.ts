2728430cf60111bd844259091a2cf53baf9ebcde
Natchi <nathan.caron@natchi.fr>
2021-08-18 17:20:30 +0200

    Fix useless typings

--- typings/handlers/EventsHandler.d.ts
+++ typings/handlers/EventsHandler.d.ts
@@ -1,10 +1,9 @@
-import { Collection } from "collection-data";
 import type { ShewenyClient } from "../index";
 export declare class EventsHandler {
     private client;
     private dir;
     constructor(client: ShewenyClient, dir: string);
-    registerAll(): Promise<Collection<string, import("../typescript/interfaces/Event").Event>>;
+    registerAll(): Promise<import("collection-data").Collection<string, import("../typescript/interfaces/Event").Event>>;
     loadAll(): Promise<void>;
     readDirAndPush(d: string): Promise<Array<string>>;
 }
