7492c190cd9c04ba4a08b592d815b2e47a431798
Raphael V <veauville.raphael@gmail.com>
2021-08-25 11:28:11 +0200

    Fix events handler

--- src/handlers/EventsHandler.ts
+++ src/handlers/EventsHandler.ts
@@ -67,7 +67,7 @@ export class EventsHandler {
     const client = this.client instanceof ShewenyClient ? this.client : this.client;
     for (const [name, evt] of evts) {
       if (evt.once) client.once(name, (...args: any[]) => evt.execute(...args));
-      else client.on(name, (...args: any[]) => evt.execute(args));
+      else client.on(name, (...args: any[]) => evt.execute(...args));
     }
   }
 }
