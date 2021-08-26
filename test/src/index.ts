8e66cd904bf1a14244a708f0e74bd49c7294fb1d
Nathan <nathan.caron@natchi.fr>
2021-08-27 00:37:04 +0200

    Update test

--- test/src/index.ts
+++ test/src/index.ts
@@ -8,7 +8,14 @@ const client = new ShewenyClient({
       type: "applications",
       guildId: "877090306103840778",
     },
+    events: {
+      directory: "./events",
+    },
   },
 });
 
 client.login("");
+
+client.handlers.commands.on("cooldownLimit", (interaction) => {
+  interaction.reply({ content: "Slow down", ephemeral: true });
+});
