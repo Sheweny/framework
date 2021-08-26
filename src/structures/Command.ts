6fb5b0f0b94677c1ebc3321408cf3b1dff5af9be
Nathan <nathan.caron@natchi.fr>
2021-08-26 13:30:59 +0200

    Add properties command

--- src/structures/Command.ts
+++ src/structures/Command.ts
@@ -1,14 +1,35 @@
+import { ApplicationCommandOptionData, PermissionString } from "discord.js";
 import type { ShewenyClient } from "../client/Client";
 import type { CommandData } from "../interfaces/Command";
 
 export abstract class Command {
   public client: ShewenyClient;
   public path: string = "";
-  public data: CommandData;
+  protected data: CommandData;
+  public name: string;
+  public type: "SLASH_COMMAND" | "CONTEXT_MENU_MESSAGE" | "CONTEXT_MENU_USER" | "MESSAGE";
+  public defaultPermission?: boolean;
+  public options?: ApplicationCommandOptionData[];
+  public category?: string;
+  public channel?: "GUILD" | "DM";
+  public cooldown?: null;
+  public adminsOnly?: boolean;
+  public userPermissions?: PermissionString[];
+  public clientPermissions?: PermissionString[];
 
   constructor(client: ShewenyClient, data: CommandData) {
     this.client = client;
     this.data = data;
+    this.name = data.name;
+    this.type = data.type;
+    this.defaultPermission = data.type !== "MESSAGE" ? data.defaultPermission : undefined;
+    this.options = data.type === "SLASH_COMMAND" ? data.options : undefined;
+    this.category = data.category;
+    this.channel = data.channel;
+    this.cooldown = data.cooldown;
+    this.adminsOnly = data.adminsOnly;
+    this.userPermissions = data.userPermissions;
+    this.clientPermissions = data.clientPermissions;
   }
 
   before?(...args: any[]): any | Promise<any>;
