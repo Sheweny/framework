57376018b33bde7a9898d27a388ea189c383de0a
Nathan <nathan.caron@natchi.fr>
2021-08-26 12:49:37 +0200

    Add exports index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { ShewenyClient } from "./client/Client";
+export { Event } from "./structures/Event";
+export { Command } from "./structures/Command";
+export { EventsManager } from "./managers/EventsManager";
+export { CommandsManager } from "./managers/CommandsManager";
