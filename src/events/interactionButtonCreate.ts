3f6e8e367a817ff1683394965050cc2ea12af095
Raphael <veauville.raphael@gmail.com>
2021-08-30 15:05:25 +0200

    Update

--- src/events/interactionButtonCreate.ts
+++ src/events/interactionButtonCreate.ts
@@ -4,27 +4,28 @@ import type { ShewenyClient } from "../client/Client";
 import type { Inhibitor } from "../structures/Inhibitor";
 
 export default async function run(client: ShewenyClient, interaction: ButtonInteraction) {
-  if (!client.collections.buttons) return;
-
-  const button = client.collections.buttons.find((value) =>
-    value.customId.includes(interaction.customId)
-  );
-
-  if (!button) return;
-  if (button.before) await button.before(interaction);
-
-  const inhibitors = client.collections.inhibitors?.filter(
-    (i: Inhibitor) => i.type.includes("BUTTON") || i.type.includes("ALL")
-  );
-
-  if (inhibitors && inhibitors.size) {
-    const sorted = [...inhibitors.values()].sort((a, b) => b.priority - a.priority);
-    for (const i of sorted) {
-      if (!i.execute(client, interaction)) return i.onFailure(client, interaction);
-    }
-  }
-
   try {
+    if (!client.collections.buttons) return;
+
+    const button = client.collections.buttons.find((value) =>
+      value.customId.includes(interaction.customId)
+    );
+
+    if (!button) return;
+
+    if (button.before) await button.before(interaction);
+
+    const inhibitors = client.collections.inhibitors?.filter(
+      (i: Inhibitor) => i.type.includes("BUTTON") || i.type.includes("ALL")
+    );
+
+    if (inhibitors && inhibitors.size) {
+      const sorted = [...inhibitors.values()].sort((a, b) => b.priority - a.priority);
+      for (const i of sorted) {
+        if (!i.execute(client, interaction)) return i.onFailure(client, interaction);
+      }
+    }
+
     await button.execute(interaction);
   } catch (e: any) {
     new ShewenyError(client, e);
--- src/events/interactionSelectMenuCreate.ts
+++ src/events/interactionSelectMenuCreate.ts
@@ -7,27 +7,27 @@ export default async function run(
   client: ShewenyClient,
   interaction: SelectMenuInteraction
 ) {
-  if (!client.collections.selectMenus) return;
-
-  const selectMenu = client.collections.selectMenus.find((value) =>
-    value.customId.includes(interaction.customId)
-  );
-
-  if (!selectMenu) return;
-  if (selectMenu.before) await selectMenu.before(interaction);
-
-  const inhibitors = client.collections.inhibitors?.filter(
-    (i: Inhibitor) => i.type.includes("SELECT_MENU") || i.type.includes("ALL")
-  );
-
-  if (inhibitors && inhibitors.size) {
-    const sorted = [...inhibitors.values()].sort((a, b) => b.priority - a.priority);
-    for (const i of sorted) {
-      if (!i.execute(client, interaction)) return i.onFailure(client, interaction);
-    }
-  }
-
   try {
+    if (!client.collections.selectMenus) return;
+
+    const selectMenu = client.collections.selectMenus.find((value) =>
+      value.customId.includes(interaction.customId)
+    );
+
+    if (!selectMenu) return;
+    if (selectMenu.before) await selectMenu.before(interaction);
+
+    const inhibitors = client.collections.inhibitors?.filter(
+      (i: Inhibitor) => i.type.includes("SELECT_MENU") || i.type.includes("ALL")
+    );
+
+    if (inhibitors && inhibitors.size) {
+      const sorted = [...inhibitors.values()].sort((a, b) => b.priority - a.priority);
+      for (const i of sorted) {
+        if (!i.execute(client, interaction)) return i.onFailure(client, interaction);
+      }
+    }
+
     await selectMenu.execute(interaction);
   } catch (e: any) {
     new ShewenyError(client, e);
