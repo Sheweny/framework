38bd161af6637cc6fb0874eb08e166ff5da2a262
Raphael <veauville.raphael@gmail.com>
2021-08-30 14:59:03 +0200

    Update

--- src/errors/ShewenyError.ts
+++ src/errors/ShewenyError.ts
@@ -6,11 +6,11 @@ const Messages: any = {
     `The property ${property} is is missing on class.\nPath : ${path}`,
 };
 export class ShewenyError extends Error {
-  constructor(client: ShewenyClient, name: string, ...args: string[]) {
+  constructor(client: ShewenyClient, err: string, ...args: string[]) {
     let message: string = "";
-    if (!name) message = "[SHEWENY_ERROR]: Unknown error";
-    if (Messages[name]) message = `[SHEWENY_ERROR]: ${Messages[name](...args)}`;
-    else if (name) message = name;
+    if (!err) message = "[SHEWENY_ERROR]: Unknown error";
+    else if (Messages[err]) message = `[SHEWENY_ERROR]: ${Messages[err](...args)}`;
+    else if (err) message = err;
     super(message);
     if (client.mode === "development") throw this;
     else client.emit("error", this);
