eb6ec5df482f7ffbfc60d043cb7f5488a5b756b0
Raphael <veauville.raphael@gmail.com>
2022-12-26 17:20:42 +0100

    New select-menus & refactor

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,21 +1,25 @@
-import { ComponentType, Interaction, InteractionType, MessageComponentInteraction } from 'discord.js';
+import { BaseInteraction, ComponentType, InteractionType as IType, MessageComponentInteraction } from 'discord.js';
 import type { ShewenyClient } from '../client/Client.js';
 
-export default function run(client: ShewenyClient, i: Interaction) {
-  if (i.type === InteractionType.MessageComponent && (i as MessageComponentInteraction).componentType === ComponentType.Button) {
-    client.emit('interactionButtonCreate', i);
+export default function run(client: ShewenyClient, i: BaseInteraction): void | boolean {
+  switch (i.type) {
+    case IType.ApplicationCommand:
+      return client.emit('interactionCommandCreate', i);
+    case IType.ApplicationCommandAutocomplete:
+      return client.emit('interactionAutocompleteCreate', i);
+    case IType.MessageComponent:
+      switch ((i as MessageComponentInteraction).componentType) {
+        case ComponentType.Button:
+          return client.emit('interactionButtonCreate', i);
+        case ComponentType.ChannelSelect:
+        case ComponentType.RoleSelect:
+        case ComponentType.StringSelect:
+        case ComponentType.UserSelect:
+        case ComponentType.SelectMenu:
+          return client.emit('interactionSelectMenuCreate', i);
+      }
+      break;
+    case IType.ModalSubmit:
+      return client.emit('interactionModalSubmitCreate', i);
   }
-
-  if (i.type === InteractionType.ApplicationCommand) client.emit('interactionCommandCreate', i);
-
-  if (i.type === InteractionType.ApplicationCommandAutocomplete) client.emit('interactionAutocompleteCreate', i);
-
-  if (
-    i.type === InteractionType.MessageComponent &&
-    (i as MessageComponentInteraction).componentType === ComponentType.SelectMenu
-  ) {
-    client.emit('interactionSelectMenuCreate', i);
-  }
-
-  if (i.type === InteractionType.ModalSubmit) client.emit('interactionModalSubmitCreate', i);
 }
