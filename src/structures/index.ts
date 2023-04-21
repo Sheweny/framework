f1d4bc3b48b655bb993c6017cdfb73508f7b8714
Veauville Raphael <veauville.raphael@gmail.com>
2023-04-21 21:17:44 +0200

    Remove old files

--- src/structures/index.ts
+++
@@ -1,7 +0,0 @@
-export { BaseStructure } from './BaseStructure.js';
-export { Button } from './Button.js';
-export { Command } from './Command.js';
-export { Event } from './Event.js';
-export { Inhibitor } from './Inhibitor.js';
-export { Modal } from './Modal.js';
-export { SelectMenu } from './SelectMenu.js';
