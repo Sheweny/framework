058e8da7c7ccf7b50f30b24c0868b7831e5ef147
Veauville Raphael <veauville.raphael@gmail.com>
2023-04-23 11:29:40 +0200

    Add test emojis

--- packages/resolve/test/resolve.test.ts
+++ packages/resolve/test/resolve.test.ts
@@ -1,27 +1,39 @@
 import { describe, test, expect } from 'vitest';
 import { DiscordResolve as resolve } from '../src';
-import { Collection, type Client, type User, type Guild, GuildChannel, Role } from 'discord.js';
+import { 
+  Collection, 
+  type Client, 
+  type User, 
+  type Guild, 
+  type GuildChannel, 
+  type Role, 
+  type GuildEmoji } from 'discord.js';
 
 type Modify<T, R> = Omit<T, keyof R> & R;
 
-type PartialClient = Partial<Modify<Client, { users: PartialCustomUserManager }>>;
-type PartialGuild = Partial<Omit<Modify<Guild, {channels: PartialCustomGuildChannelManager, roles: PartialCustomRoleManager}>, 'valueOf'>>;
+type PartialClient = Partial<Modify<Client, { users: PartialCustomUserManager, guilds: PartialCustomGuildManager }>>;
+type PartialGuild = Partial<Omit<Modify<Guild, {channels: PartialCustomGuildChannelManager, roles: PartialCustomRoleManager, emojis: PartialCustomGuildEmojiManager}>, 'valueOf'>>;
 
 
 // Partial managers (partials managers without valueOf)
 type PartialCustomUserManager = Partial<Omit<CustomUsermanager, 'valueOf'>>;
+type PartialCustomGuildManager = Partial<Omit<CustomGuildmanager, 'valueOf'>>;
 type PartialCustomGuildChannelManager = Partial<Omit<CustomGuildChannelManager, 'valueOf'>>;
+type PartialCustomGuildEmojiManager = Partial<Omit<CustomGuildEmojiManager, 'valueOf'>>;
 type PartialCustomRoleManager = Partial<Omit<CustomRoleManager, 'valueOf'>>;
 
 // Custom managers (managers with custom cache)
 type CustomUsermanager = Modify<Client['users'], { cache: Collection<string, PartialUser> }>;
+type CustomGuildmanager = Modify<Client['guilds'], { cache: Collection<string, PartialGuild> }>;
 type CustomGuildChannelManager = Modify<Guild['channels'], { cache: Collection<string, PartialGuildChannel> }>;
+type CustomGuildEmojiManager = Modify<Guild['emojis'], { cache: Collection<string, PartialGuildEmoji> }>;
 type CustomRoleManager = Modify<Guild['roles'], { cache: Collection<string, PartialRole> }>;
 
 
 // Partial structures (channels, roles, users, etc.) without toString and valueOf (discord.js custom methods)
 type PartialUser = Partial<Omit<User, 'toString' | 'valueOf'>>;
 type PartialGuildChannel = Partial<Omit<GuildChannel, 'toString' | 'valueOf'>>;
+type PartialGuildEmoji = Partial<Omit<GuildEmoji, 'toString' | 'valueOf'>>;
 type PartialRole = Partial<Omit<Role, 'toString' | 'valueOf'>>;
 
 
@@ -36,6 +48,15 @@ const client: PartialClient = {
       ['000000000000000004', { id: '000000000000000004', username: 'user55555', discriminator: '5555' }],
     ]),
   },
+  guilds: {
+    cache: new Collection<string, PartialGuild>([
+      ['000000000000000000', { id: '000000000000000000', name: 'guild1' }],
+      ['000000000000000001', { id: '000000000000000001', name: 'guild22' }],
+      ['000000000000000002', { id: '000000000000000002', name: 'guild333' }],
+      ['000000000000000003', { id: '000000000000000003', name: 'guild4444' }],
+      ['000000000000000004', { id: '000000000000000004', name: 'guild55555' }],
+    ]),
+  }
 };
 
 
@@ -58,12 +79,21 @@ const guild: PartialGuild = {
       ['000000000000000004', { id: '000000000000000004', name: 'channel55555' }],
     ]),
   },
+  emojis: {
+    cache: new Collection<string, PartialGuildEmoji>([
+      ['000000000000000000', { id: '000000000000000000', name: 'emoji1' }],
+      ['000000000000000001', { id: '000000000000000001', name: 'emoji22' }],
+      ['000000000000000002', { id: '000000000000000002', name: 'emoji333' }],
+      ['000000000000000003', { id: '000000000000000003', name: 'emoji4444' }],
+      ['000000000000000004', { id: '000000000000000004', name: 'emoji55555' }],
+    ]),
+  }
 };
 
 const fakeClient = client as unknown as Client;
 const fakeGuild = guild as unknown as Guild;
 
-describe('resolveRole', () => {
+describe('resolveRole (by id, mention, name, start of name)', () => {
   test('finds the role by ID', () => {
     expect(resolve.resolveRole(fakeGuild, '000000000000000000')).toEqual({ id: '000000000000000000', name: 'role1' });
     expect(resolve.resolveRole(fakeGuild, '000000000000000001')).toEqual({ id: '000000000000000001', name: 'role22' });
@@ -91,7 +121,7 @@ describe('resolveRole', () => {
   });
 });
 
-describe('resolveUser', () => {
+describe('resolveUser (by id, mention, name, name & discriminator, start of name)', () => {
   test('finds the user by ID', async () => {
     expect(await resolve.resolveUser(fakeClient, '000000000000000000')).toEqual({id: '000000000000000000', username: 'user1', discriminator: '1111',});
     expect(await resolve.resolveUser(fakeClient, '000000000000000001')).toEqual({id: '000000000000000001', username: 'user22', discriminator: '2222'});
@@ -117,7 +147,8 @@ describe('resolveUser', () => {
     expect(await resolve.resolveUser(fakeClient, 'user2')).toEqual({id: '000000000000000001', username: 'user22', discriminator: '2222', });
     expect(await resolve.resolveUser(fakeClient, '000')).toBeUndefined();
   });
-  describe('resolveChannel', () => {
+});
+describe('resolveChannel (by id, mention, name, start of name)', () => {
   test('finds the channel by ID', () => {
     expect(resolve.resolveChannel(fakeGuild, '000000000000000000')).toEqual({ id: '000000000000000000', name: 'channel1' });
     expect(resolve.resolveChannel(fakeGuild, '000000000000000001')).toEqual({ id: '000000000000000001', name: 'channel22' });
@@ -144,4 +175,39 @@ describe('resolveUser', () => {
     expect(resolve.resolveChannel(fakeGuild, 'channel0')).toBeUndefined();
   });
 });
+
+describe('resolveGuild (by name & id)', () => {
+  test('finds the guild by ID', () => {
+    expect(resolve.resolveGuild(fakeClient, '000000000000000000')).toEqual({ id: '000000000000000000', name: 'guild1' });
+    expect(resolve.resolveGuild(fakeClient, '000000000000000001')).toEqual({ id: '000000000000000001', name: 'guild22' });
+    expect(resolve.resolveGuild(fakeClient, '000')).toBeUndefined();
+  });
+  test('finds the guild by name', () => {
+    expect(resolve.resolveGuild(fakeClient, 'guild1')).toEqual({ id: '000000000000000000', name: 'guild1' });
+    expect(resolve.resolveGuild(fakeClient, 'guild22')).toEqual({ id: '000000000000000001', name: 'guild22' });
+    expect(resolve.resolveGuild(fakeClient, 'guild0')).toBeUndefined();
+  });
 });
+
+describe('resolveGuildEmoji (by id, name, mention)', () => {
+  test('finds the emoji by ID', () => {
+    expect(resolve.resolveGuildEmoji(fakeGuild, '000000000000000000')).toEqual({ id: '000000000000000000', name: 'emoji1' });
+    expect(resolve.resolveGuildEmoji(fakeGuild, '000000000000000001')).toEqual({ id: '000000000000000001', name: 'emoji22' });
+    expect(resolve.resolveGuildEmoji(fakeGuild, '000')).toBeUndefined();
+  });
+  test('finds the emoji by name', () => {
+    expect(resolve.resolveGuildEmoji(fakeGuild, 'emoji1')).toEqual({ id: '000000000000000000', name: 'emoji1' });
+    expect(resolve.resolveGuildEmoji(fakeGuild, 'emoji22')).toEqual({ id: '000000000000000001', name: 'emoji22' });
+    expect(resolve.resolveGuildEmoji(fakeGuild, 'emoji0')).toBeUndefined();
+  });
+  test('finds the (normal) emoji by mention', () => {
+    expect(resolve.resolveGuildEmoji(fakeGuild, '<:emoji1:000000000000000000>')).toEqual({ id: '000000000000000000', name: 'emoji1' });
+    expect(resolve.resolveGuildEmoji(fakeGuild, '<:emoji22:000000000000000001>')).toEqual({ id: '000000000000000001', name: 'emoji22' });
+    expect(resolve.resolveGuildEmoji(fakeGuild, '<:emoji0:000>')).toBeUndefined();
+  });
+  test('finds the (animated) emoji by mention', () => {
+    expect(resolve.resolveGuildEmoji(fakeGuild, '<a:emoji1:000000000000000000>')).toEqual({ id: '000000000000000000', name: 'emoji1' });
+    expect(resolve.resolveGuildEmoji(fakeGuild, '<a:emoji22:000000000000000001>')).toEqual({ id: '000000000000000001', name: 'emoji22' });
+    expect(resolve.resolveGuildEmoji(fakeGuild, '<a:emoji0:000>')).toBeUndefined();
+  });
+})
